29816799376df1f25dcdea26df88e50c98c19f6f
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-08-12 01:56:21 -0400

    Adds an automated test for UC.RE5 - a really basic example test of checking on a temperature sensor. Works with the fake temperature sensor mock.

--- testing/features/step_definitions/webhooks.rb
+++ testing/features/step_definitions/webhooks.rb
@@ -57,3 +57,48 @@ Then(/^I receive a webhook message confirming the Rhizome shutdown Pump (\d+) on
   expect(webhook_result[:stopTime]).to eq stop_time_arg
   # We're assuming that the shutoff method doesn't wipe the stopTime value as shutting off manually does.
 end
+
+And(/^the Rhizome has a webhook for Temperature Sensor (\d+)$/) do |temp_id|
+  if @particle_client.webhooks.any? { |wh| wh.event == "temps/#{temp_id}" && wh.url == "#{@global_settings[:endpoint]}/temps" }
+    @pump_webhook = @particle_client.webhooks.find { |wh| wh.event == "temps/#{temp_id}" && wh.url == "#{@global_settings[:endpoint]}/temps" }
+  else
+    expect {
+      @pump_webhook = @particle_client.webhook(
+          mydevices: true,
+          deviceid: @rhizome.id,
+          event: "temps/#{temp_id}",
+          url: "#{@global_settings[:endpoint]}/temps",
+          json: {
+              id:           '{{id}}',
+              current_temp: '{{current_temp}}',
+              rhizome:      '{{SPARK_CORE_ID}}'
+          }).create
+    }.to_not raise_exception
+  end
+end
+
+When(/^I wait for a webhook message from Temperature Sensor (\d+) for no longer than (\d+) seconds$/) do |temp_id, wait_time|
+  start_time = Time.now
+
+  get_result = HTTParty.get("#{@global_settings[:endpoint]}/last/temps")
+                       .parsed_response
+  webhook_result = JSON.parse(get_result,
+                              symbolize_names: true)
+
+  # If we haven't gotten a reading yet, try again every 5 seconds until we pass the wait time
+  while webhook_result[:current_temp].nil? do
+    expect(Time.now.sec - start_time.sec).to be <= wait_time.to_i
+    sleep 5
+    get_result = HTTParty.get("#{@global_settings[:endpoint]}/last/temps")
+                         .parsed_response
+    webhook_result = JSON.parse(get_result,
+                                symbolize_names: true)
+  end
+
+  @last_temp_reading = Array.new
+  @last_temp_reading[temp_id.to_i] = webhook_result[:current_temp].to_f
+end
+
+Then(/^the temperature reading for Temperature Sensor (\d+) is within (.*) degrees of (.*) degrees Celsius/) do |temp_id, range, expected_temp|
+  expect(@last_temp_reading[temp_id.to_i]).to be_within(range.to_f).of(expected_temp.to_f)
+end
